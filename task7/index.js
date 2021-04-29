cbed77b06f8ffa8a774c6f4b93bd85ead8c23269
DESKTOP-D73SHNV\Serj <donserhio@tut.by>
2021-04-29 11:11:32 +0300

    Done seventh lab.

---
+++ task7/index.js
@@ -0,0 +1,532 @@
+const list = [{
+        "id": "c24cf51e-7c6c-47d8-a56f-8d11fc7765e7",
+        "parent_id": null
+    },
+    {
+        "id": "3d537081-374e-47f0-aeb8-a9ba9cfe1eb0",
+        "parent_id": "c24cf51e-7c6c-47d8-a56f-8d11fc7765e7"
+    },
+    {
+        "id": "b1e50808-6a38-4fcd-84fa-ac64e037b252",
+        "parent_id": "3d537081-374e-47f0-aeb8-a9ba9cfe1eb0"
+    },
+    {
+        "id": "39775183-bfd5-4f28-8600-476a91b7b937",
+        "parent_id": "b1e50808-6a38-4fcd-84fa-ac64e037b252"
+    },
+    {
+        "id": "c9c7f41f-5bdf-4e76-8945-b656ee08a4f0",
+        "parent_id": "39775183-bfd5-4f28-8600-476a91b7b937"
+    },
+    {
+        "id": "f47a1069-9dcd-4917-90e8-ebfa6772d480",
+        "parent_id": "39775183-bfd5-4f28-8600-476a91b7b937"
+    },
+    {
+        "id": "859d226c-ea15-45a0-a10b-56da3db1a940",
+        "parent_id": "39775183-bfd5-4f28-8600-476a91b7b937"
+    },
+    {
+        "id": "780b0eb7-1cab-4ed8-9184-69d140d9fa88",
+        "parent_id": "39775183-bfd5-4f28-8600-476a91b7b937"
+    },
+    {
+        "id": "f47ce8ac-9439-44c6-ab35-0d6001ec060e",
+        "parent_id": "39775183-bfd5-4f28-8600-476a91b7b937"
+    },
+    {
+        "id": "5e25a07f-ac6d-4f3b-80b1-6fcacc744d51",
+        "parent_id": "39775183-bfd5-4f28-8600-476a91b7b937"
+    },
+    {
+        "id": "bcf7d90d-bb9f-4c05-ad35-91645041e8bd",
+        "parent_id": "39775183-bfd5-4f28-8600-476a91b7b937"
+    },
+    {
+        "id": "e3f02312-2c37-4efc-8de7-6ab534c1ed4c",
+        "parent_id": "b1e50808-6a38-4fcd-84fa-ac64e037b252"
+    },
+    {
+        "id": "3e01018b-3121-495a-9daa-13f063cafc6b",
+        "parent_id": "e3f02312-2c37-4efc-8de7-6ab534c1ed4c"
+    },
+    {
+        "id": "3cc99e44-d300-4b79-8e4e-d76fd72e0448",
+        "parent_id": "e3f02312-2c37-4efc-8de7-6ab534c1ed4c"
+    },
+    {
+        "id": "6b4cafac-5fac-4f7d-ae5e-e3117d657690",
+        "parent_id": "e3f02312-2c37-4efc-8de7-6ab534c1ed4c"
+    },
+    {
+        "id": "2d4c5264-26ae-4ce8-b336-09d7c68e742d",
+        "parent_id": "e3f02312-2c37-4efc-8de7-6ab534c1ed4c"
+    },
+    {
+        "id": "cbe1ae84-f6a4-4540-b4a0-e583bdb239bc",
+        "parent_id": "e3f02312-2c37-4efc-8de7-6ab534c1ed4c"
+    },
+    {
+        "id": "66b2cceb-6f00-4d62-bfc1-85b5d1c6021c",
+        "parent_id": "e3f02312-2c37-4efc-8de7-6ab534c1ed4c"
+    },
+    {
+        "id": "27c7c2fc-f862-4887-91c8-7f9e0221a6f1",
+        "parent_id": "e3f02312-2c37-4efc-8de7-6ab534c1ed4c"
+    },
+    {
+        "id": "68620c80-d3aa-450f-b377-be373da18f7e",
+        "parent_id": "e3f02312-2c37-4efc-8de7-6ab534c1ed4c"
+    },
+    {
+        "id": "becf23a3-5a30-4492-a5b5-40ce6ccc64c5",
+        "parent_id": "3d537081-374e-47f0-aeb8-a9ba9cfe1eb0"
+    },
+    {
+        "id": "e232a8f1-00e4-46ca-9772-e4ea28009aaf",
+        "parent_id": "becf23a3-5a30-4492-a5b5-40ce6ccc64c5"
+    },
+    {
+        "id": "db672af6-f6d8-47bc-8513-788a58ea72b9",
+        "parent_id": "e232a8f1-00e4-46ca-9772-e4ea28009aaf"
+    },
+    {
+        "id": "27c24584-8d42-4485-9661-6984f842e0c2",
+        "parent_id": "e232a8f1-00e4-46ca-9772-e4ea28009aaf"
+    },
+    {
+        "id": "f35e2312-8080-4515-b209-8fd9c6503510",
+        "parent_id": "e232a8f1-00e4-46ca-9772-e4ea28009aaf"
+    },
+    {
+        "id": "b0a9997e-51d5-4313-97df-e5bfdbbbae8f",
+        "parent_id": "e232a8f1-00e4-46ca-9772-e4ea28009aaf"
+    },
+    {
+        "id": "3b0d408b-012b-4d85-adb1-27086d36f5a8",
+        "parent_id": "e232a8f1-00e4-46ca-9772-e4ea28009aaf"
+    },
+    {
+        "id": "ae1b5b81-6ddd-40f7-9c20-418d28e2a2c4",
+        "parent_id": "e232a8f1-00e4-46ca-9772-e4ea28009aaf"
+    },
+    {
+        "id": "c8e1cd03-8bc7-4149-a91d-713ff45bb397",
+        "parent_id": "3d537081-374e-47f0-aeb8-a9ba9cfe1eb0"
+    },
+    {
+        "id": "7a1911ca-a241-4a0c-8e86-cf13669b1589",
+        "parent_id": "c8e1cd03-8bc7-4149-a91d-713ff45bb397"
+    },
+    {
+        "id": "2024a869-14f7-4e06-99b7-aedfa8594794",
+        "parent_id": "7a1911ca-a241-4a0c-8e86-cf13669b1589"
+    },
+    {
+        "id": "58aa65dc-edf3-4859-910e-0e4ada62ed53",
+        "parent_id": "7a1911ca-a241-4a0c-8e86-cf13669b1589"
+    },
+    {
+        "id": "e07c2013-5e30-420c-9dfc-ca1059c27673",
+        "parent_id": "7a1911ca-a241-4a0c-8e86-cf13669b1589"
+    },
+    {
+        "id": "c6c2989c-843e-4c82-be3d-41ac971307c4",
+        "parent_id": "7a1911ca-a241-4a0c-8e86-cf13669b1589"
+    },
+    {
+        "id": "9883c7cb-3e2a-4f97-afb1-e61ece402082",
+        "parent_id": "3d537081-374e-47f0-aeb8-a9ba9cfe1eb0"
+    },
+    {
+        "id": "cf074c67-04f2-4d2f-a301-f7ba14857b50",
+        "parent_id": "9883c7cb-3e2a-4f97-afb1-e61ece402082"
+    },
+    {
+        "id": "54413971-0386-4d06-9e3c-8334cc700cb5",
+        "parent_id": "cf074c67-04f2-4d2f-a301-f7ba14857b50"
+    },
+    {
+        "id": "37a04159-948c-4889-bdf2-e6dd2b8cc00a",
+        "parent_id": "cf074c67-04f2-4d2f-a301-f7ba14857b50"
+    },
+    {
+        "id": "015ac3d2-e14d-4ecb-b366-b806c4a9b954",
+        "parent_id": "cf074c67-04f2-4d2f-a301-f7ba14857b50"
+    },
+    {
+        "id": "2ac5825a-11cf-4584-8836-5da6bd4556ce",
+        "parent_id": "cf074c67-04f2-4d2f-a301-f7ba14857b50"
+    },
+    {
+        "id": "fc6c88f8-39e7-45b3-b0ad-42ab68c5641b",
+        "parent_id": "cf074c67-04f2-4d2f-a301-f7ba14857b50"
+    },
+    {
+        "id": "940ad98e-349d-4bf6-a9ba-6f4ee8017265",
+        "parent_id": "cf074c67-04f2-4d2f-a301-f7ba14857b50"
+    },
+    {
+        "id": "70ce99ff-e8af-4d18-a976-3333fee0a0fe",
+        "parent_id": "cf074c67-04f2-4d2f-a301-f7ba14857b50"
+    },
+    {
+        "id": "dbd192c0-7d2b-424f-bd6d-c3d7b8b2a400",
+        "parent_id": "9883c7cb-3e2a-4f97-afb1-e61ece402082"
+    },
+    {
+        "id": "4cce5caf-7820-48b3-b177-84717442ae31",
+        "parent_id": "dbd192c0-7d2b-424f-bd6d-c3d7b8b2a400"
+    },
+    {
+        "id": "0dc654f5-f5c5-406c-b6e6-64ba3325ce02",
+        "parent_id": "dbd192c0-7d2b-424f-bd6d-c3d7b8b2a400"
+    },
+    {
+        "id": "81446dd5-fa62-43ce-a749-d659af944da8",
+        "parent_id": "dbd192c0-7d2b-424f-bd6d-c3d7b8b2a400"
+    },
+    {
+        "id": "ac53d34d-533e-46a5-b207-1a00a281a5c4",
+        "parent_id": "dbd192c0-7d2b-424f-bd6d-c3d7b8b2a400"
+    },
+    {
+        "id": "e9627bc2-4d71-43e1-81e6-3cc098626ba8",
+        "parent_id": "dbd192c0-7d2b-424f-bd6d-c3d7b8b2a400"
+    },
+    {
+        "id": "1621d1da-be78-4674-9a2d-3c846c8acb67",
+        "parent_id": "dbd192c0-7d2b-424f-bd6d-c3d7b8b2a400"
+    },
+    {
+        "id": "f3fde68a-1bba-4534-b3e3-240a2bb6650e",
+        "parent_id": "dbd192c0-7d2b-424f-bd6d-c3d7b8b2a400"
+    },
+    {
+        "id": "33f32215-9f09-424d-a43c-9955c5f80c88",
+        "parent_id": "c24cf51e-7c6c-47d8-a56f-8d11fc7765e7"
+    },
+    {
+        "id": "d1fa8f7d-a68c-41f2-803d-e9255adc26be",
+        "parent_id": "33f32215-9f09-424d-a43c-9955c5f80c88"
+    },
+    {
+        "id": "5b114f39-28fb-4771-948d-a1a5f7826600",
+        "parent_id": "d1fa8f7d-a68c-41f2-803d-e9255adc26be"
+    },
+    {
+        "id": "7e2603d7-3720-4738-966d-f91553399d54",
+        "parent_id": "5b114f39-28fb-4771-948d-a1a5f7826600"
+    },
+    {
+        "id": "cb012666-4322-4e90-ae5d-27937e90548e",
+        "parent_id": "5b114f39-28fb-4771-948d-a1a5f7826600"
+    },
+    {
+        "id": "b9692699-de73-4912-be75-b0b08efbee9d",
+        "parent_id": "5b114f39-28fb-4771-948d-a1a5f7826600"
+    },
+    {
+        "id": "d053eada-30a2-4d9d-98ac-60ca0b8ab7e0",
+        "parent_id": "5b114f39-28fb-4771-948d-a1a5f7826600"
+    },
+    {
+        "id": "6eb0b8bb-7411-4f9e-8a38-69ca5c23924f",
+        "parent_id": "5b114f39-28fb-4771-948d-a1a5f7826600"
+    },
+    {
+        "id": "90b7cf1a-66c3-4c2e-85ef-e2992e9b1a72",
+        "parent_id": "5b114f39-28fb-4771-948d-a1a5f7826600"
+    },
+    {
+        "id": "92b04fbb-52de-44e4-8e22-f9b57463f8ab",
+        "parent_id": "5b114f39-28fb-4771-948d-a1a5f7826600"
+    },
+    {
+        "id": "6c57e11f-f413-4b66-ab64-986997268b5a",
+        "parent_id": "5b114f39-28fb-4771-948d-a1a5f7826600"
+    },
+    {
+        "id": "203daf8f-d81d-4646-babb-d8cc19b6afd0",
+        "parent_id": "5b114f39-28fb-4771-948d-a1a5f7826600"
+    },
+    {
+        "id": "9033c281-572f-487a-a6f5-3e2e28f9daed",
+        "parent_id": "5b114f39-28fb-4771-948d-a1a5f7826600"
+    },
+    {
+        "id": "808b7175-1059-4556-ae48-648ab7cc715d",
+        "parent_id": "5b114f39-28fb-4771-948d-a1a5f7826600"
+    },
+    {
+        "id": "44ee9886-0e6b-4d39-bf8c-2316894d7e7c",
+        "parent_id": "5b114f39-28fb-4771-948d-a1a5f7826600"
+    },
+    {
+        "id": "ae009e97-a46f-489e-8470-c165a69c95a7",
+        "parent_id": "5b114f39-28fb-4771-948d-a1a5f7826600"
+    },
+    {
+        "id": "17ee9c74-ac5a-49c2-8d54-8cbeadb06bda",
+        "parent_id": "5b114f39-28fb-4771-948d-a1a5f7826600"
+    },
+    {
+        "id": "c2014c4e-ca59-435e-9afc-3280579699ec",
+        "parent_id": "33f32215-9f09-424d-a43c-9955c5f80c88"
+    },
+    {
+        "id": "19b57ded-496e-4ace-b209-da01b06b3fe8",
+        "parent_id": "c2014c4e-ca59-435e-9afc-3280579699ec"
+    },
+    {
+        "id": "96526689-2ddd-4455-b16b-c8f431870c4b",
+        "parent_id": "19b57ded-496e-4ace-b209-da01b06b3fe8"
+    },
+    {
+        "id": "03aae0e4-ec29-4cb7-af4e-3a11f6dcd3d0",
+        "parent_id": "19b57ded-496e-4ace-b209-da01b06b3fe8"
+    },
+    {
+        "id": "aaef0760-c1cd-40ec-ac18-ccc4947a66dd",
+        "parent_id": "19b57ded-496e-4ace-b209-da01b06b3fe8"
+    },
+    {
+        "id": "8b1ba870-2a2a-4a1b-9c45-c9fcc082bd59",
+        "parent_id": "19b57ded-496e-4ace-b209-da01b06b3fe8"
+    },
+    {
+        "id": "821c52ef-b7da-4df3-bce7-1990519ed376",
+        "parent_id": "19b57ded-496e-4ace-b209-da01b06b3fe8"
+    },
+    {
+        "id": "514244d3-a994-4946-aa83-8a3d339d9b2e",
+        "parent_id": "19b57ded-496e-4ace-b209-da01b06b3fe8"
+    },
+    {
+        "id": "b7b4f2ef-0d91-46d3-b4b1-fa8cfeb9f084",
+        "parent_id": "19b57ded-496e-4ace-b209-da01b06b3fe8"
+    },
+    {
+        "id": "0a696537-e76d-4950-875c-a3dee0dfadb7",
+        "parent_id": "33f32215-9f09-424d-a43c-9955c5f80c88"
+    },
+    {
+        "id": "78044db2-fec7-4d69-a456-3f8e5a61b623",
+        "parent_id": "0a696537-e76d-4950-875c-a3dee0dfadb7"
+    },
+    {
+        "id": "0975f5bf-8f4a-4c7e-92be-8f282719244e",
+        "parent_id": "78044db2-fec7-4d69-a456-3f8e5a61b623"
+    },
+    {
+        "id": "1b87abe5-7343-4bd5-a78a-c7fa3499da91",
+        "parent_id": "78044db2-fec7-4d69-a456-3f8e5a61b623"
+    },
+    {
+        "id": "16a871e3-23fd-44d9-b09e-ff537c685366",
+        "parent_id": "78044db2-fec7-4d69-a456-3f8e5a61b623"
+    },
+    {
+        "id": "a1a87330-7397-4bf3-b887-cac6a30238da",
+        "parent_id": "78044db2-fec7-4d69-a456-3f8e5a61b623"
+    },
+    {
+        "id": "44573a2d-724d-4f6d-8e71-02d1f40437a4",
+        "parent_id": "33f32215-9f09-424d-a43c-9955c5f80c88"
+    },
+    {
+        "id": "416ad4f7-c4c0-475b-9410-d8ed7551d7e4",
+        "parent_id": "44573a2d-724d-4f6d-8e71-02d1f40437a4"
+    },
+    {
+        "id": "bee40d36-5f75-4f96-aa64-780f99330be6",
+        "parent_id": "416ad4f7-c4c0-475b-9410-d8ed7551d7e4"
+    },
+    {
+        "id": "e8e14bc3-0c9a-4a34-87bd-704faa32ce02",
+        "parent_id": "416ad4f7-c4c0-475b-9410-d8ed7551d7e4"
+    },
+    {
+        "id": "d4ccf4fe-b95e-4e49-9b45-3b3c01c62835",
+        "parent_id": "416ad4f7-c4c0-475b-9410-d8ed7551d7e4"
+    },
+    {
+        "id": "3df1da5c-6b63-4ddc-bfb4-eda8046be233",
+        "parent_id": "416ad4f7-c4c0-475b-9410-d8ed7551d7e4"
+    },
+    {
+        "id": "891c5e0d-97d1-462e-9941-3ad569873933",
+        "parent_id": "416ad4f7-c4c0-475b-9410-d8ed7551d7e4"
+    },
+    {
+        "id": "98c80033-2153-4cfd-b611-65a240dec89b",
+        "parent_id": "416ad4f7-c4c0-475b-9410-d8ed7551d7e4"
+    },
+    {
+        "id": "9007fa26-8ef2-43cb-800f-effd41177b45",
+        "parent_id": "416ad4f7-c4c0-475b-9410-d8ed7551d7e4"
+    },
+    {
+        "id": "a8e9b2d7-6e17-4101-8625-63adc4455a70",
+        "parent_id": "416ad4f7-c4c0-475b-9410-d8ed7551d7e4"
+    },
+    {
+        "id": "d807cdab-a10c-43df-9a80-16e8e0af131d",
+        "parent_id": "416ad4f7-c4c0-475b-9410-d8ed7551d7e4"
+    },
+    {
+        "id": "34eb5a91-2306-4991-ad0b-0fd8dc8a927a",
+        "parent_id": "416ad4f7-c4c0-475b-9410-d8ed7551d7e4"
+    },
+    {
+        "id": "276716bc-5d9c-4712-af18-54772aea4f10",
+        "parent_id": "416ad4f7-c4c0-475b-9410-d8ed7551d7e4"
+    },
+    {
+        "id": "c26b4a9e-b851-4ae0-949b-47dba3ff6602",
+        "parent_id": "44573a2d-724d-4f6d-8e71-02d1f40437a4"
+    },
+    {
+        "id": "20e45884-c44a-4c16-8524-724242eb4dd6",
+        "parent_id": "c26b4a9e-b851-4ae0-949b-47dba3ff6602"
+    },
+    {
+        "id": "a6264be8-1e6f-4866-8664-e009423e97b2",
+        "parent_id": "c24cf51e-7c6c-47d8-a56f-8d11fc7765e7"
+    },
+    {
+        "id": "894c0544-991f-4508-ab71-d0959ae1aa53",
+        "parent_id": "a6264be8-1e6f-4866-8664-e009423e97b2"
+    },
+    {
+        "id": "fa145b9d-1a14-4aa7-ac6f-9e148fa39f74",
+        "parent_id": "894c0544-991f-4508-ab71-d0959ae1aa53"
+    },
+    {
+        "id": "5ce7fedf-cd6c-499a-b688-d2d98ca96b04",
+        "parent_id": "fa145b9d-1a14-4aa7-ac6f-9e148fa39f74"
+    },
+    {
+        "id": "6a58786f-f87a-4979-8bec-d81d048cefd6",
+        "parent_id": "fa145b9d-1a14-4aa7-ac6f-9e148fa39f74"
+    }
+];
+const rootId = 'c24cf51e-7c6c-47d8-a56f-8d11fc7765e7';
+
+class Tree {
+    state = {};
+    constructor(flatList, rootActivityId) {
+        this.initTree(flatList, rootActivityId);
+    }
+
+    initTree(data, rootActivityId) {
+        this.state = {
+            id: rootActivityId,
+            parent_id: null,
+            children: this.generateChildrenForId(rootActivityId, data)
+        }
+    }
+
+    generateChildrenForId(id, data) {
+        let res = [];
+        data.map((a) => {
+            if (a.parent_id === id) {
+                res.push({
+                    id: a.id,
+                    parent_id: a.parent_id,
+                    children: this.generateChildrenForId(a.id, data)
+                })
+            }
+        })
+        return res;
+    }
+}
+
+const tree = new Tree(list, rootId);
+console.log(tree.state)
+
+
+function getNodeById(id) {
+    if (tree.state.id === id) {
+        return tree.state
+    } else {
+        return getChildById(tree.state.children)
+    }
+
+    function getChildById(childs) {
+        let currentChild = {};
+
+        for (const child of childs) {
+            if (child.id === id) {
+                return child
+            } else {
+                currentChild = getChildById(child.children)
+            }
+        }
+        return currentChild
+    }
+}
+
+
+function createNewNode(id, parentId, nodeInfo) {
+
+    let node = getNodeById(id)
+    if (Object.keys(node).length !== 0) {
+        throw console.error('Already created!');
+    } else {
+        let parentNode = getNodeById(parentId)
+
+        let newNode = {
+            id,
+            parentId,
+        }
+        parentNode.children.push(Object.assign(newNode, nodeInfo))
+    }
+}
+
+
+function updateNodeById(id, updateProperties) {
+    let node = getNodeById(id);
+    if (Object.keys(node).length == 0) {
+        throw console.error('Can\'t update!');
+    } else {
+        node = Object.assign(node, updateProperties)
+    }
+}
+
+
+function deleteNodeById(id) {
+    let node = getNodeById(id)
+    let parentNode = getNodeById(node.parent_id)
+    parentNode.children = parentNode.children.filter(node => node.id !== id)
+}
+
+
+function getParent(id) {
+    return getNodeById(getNodeById(id).parent_id)
+}
+
+
+function getSiblings(id) {
+    return getParent(id).children.filter(node => node.id !== id)
+}
+
+
+/*Quick sort*/
+const qsort = (arr) => {
+    if (arr.length < 2) {
+        return arr
+    }
+    ref = arr[0]
+    const less = [];
+    const greater = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (ref > arr[i]) {
+            less.push(arr[i])
+        } else {
+            greater.push(arr[i])
+        }
+    }
+    return qsort(less).concat(ref, qsort(greater))
+}
